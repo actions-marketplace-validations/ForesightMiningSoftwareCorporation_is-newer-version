2b9f90a81fef7141f034cbf1b5479a6dbed69bd0
lois.postula <lois.postula@foresightmining.com>
2023-04-10 18:22:33 +0200

    feat: implement is-newer-version action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,115 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import { parseReleaseType, isNewerVersion } from '../src/version';
+import { expect, describe, it } from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+describe('parseReleaseType', () => {
+  it('should return the release type when a valid release type is provided', () => {
+    expect(parseReleaseType('major')).toBe('major');
+  });
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+  it('should throw an error when an invalid release type is provided', () => {
+    expect(() => parseReleaseType('invalid')).toThrow('Invalid release type');
+  });
+});
+
+describe('isNewerVersion', () => {
+  it('should throw an error when the release type is invalid', () => {
+    expect(() => isNewerVersion('1.0.0', '1.1.0', 'invalid')).toThrow(
+      'Invalid release type: invalid'
+    );
+  });
+
+  it('should throw an error when the before parameter is invalid', () => {
+    expect(() => isNewerVersion('invalid', '1.1.0', 'minor')).toThrow(
+      'Cannot infer valid semver version from `before` input: invalid'
+    );
+  });
+
+  it('should throw an error when the before parameter is not set', () => {
+    expect(() => isNewerVersion('', '1.1.0', 'minor')).toThrow(
+      'Cannot infer valid semver version from empty `before` input'
+    );
+  });
+
+  it('should throw an error when the after parameter is invalid', () => {
+    expect(() => isNewerVersion('1.1.0', 'invalid', 'minor')).toThrow(
+      'Cannot infer valid semver version from `after` input: invalid'
+    );
+  });
+
+  it('should throw an error when the before parameter is not set', () => {
+    expect(() => isNewerVersion('1.1.0', '', 'minor')).toThrow(
+      'Cannot infer valid semver version from empty `after` input'
+    );
+  });
+
+  it('should return true when the after version is after the before version', () => {
+    expect(isNewerVersion('1.0.0', '1.1.0', 'minor')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version', () => {
+    expect(isNewerVersion('1.1.0', '1.0.0', 'minor')).toBe(false);
+  });
+
+  for (const releaseType of ['major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'prerelease']) {
+    it(`should return false when the after version is the same as the before version for release type ${releaseType}`, () => {
+      expect(isNewerVersion('1.0.0', '1.0.0', releaseType)).toBe(false);
+    });
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+
+  it('should return true when the after version is after the before version with "major" release type', () => {
+    expect(isNewerVersion('1.0.0', '2.0.0', 'major')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with "major" release type', () => {
+    expect(isNewerVersion('2.0.0', '1.0.0', 'major')).toBe(false);
+  });
+
+  it('should return true when the after version is after the before version with "premajor" release type', () => {
+    expect(isNewerVersion('1.0.0', '2.0.0-0', 'premajor')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with "premajor" release type', () => {
+    expect(isNewerVersion('2.0.0-0', '1.0.0', 'premajor')).toBe(false);
+  });
+
+  it('should return true when the after version is after the before version with "minor" release type', () => {
+    expect(isNewerVersion('1.0.0', '1.1.0', 'minor')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with "minor" release type', () => {
+    expect(isNewerVersion('1.1.0', '1.0.0', 'minor')).toBe(false);
+  });
+
+  it('should return true when the after version is after the before version with "preminor" release type', () => {
+    expect(isNewerVersion('1.0.0', '1.1.0-0', 'preminor')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with "preminor" release type', () => {
+    expect(isNewerVersion('1.1.0-0', '1.0.0', 'preminor')).toBe(false);
+  });
+
+  it('should return true when the after version is after the before version with "patch" release type', () => {
+    expect(isNewerVersion('1.0.0', '1.0.1', 'patch')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with "patch" release type', () => {
+    expect(isNewerVersion('1.0.1', '1.0.0', 'patch')).toBe(false);
+  });
+
+  it('should return true when the after version is after the before version with "prepatch" release type', () => {
+    expect(isNewerVersion('1.0.0', '1.0.1-0', 'prepatch')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with "prepatch" release type', () => {
+    expect(isNewerVersion('1.0.1-0', '1.0.0', 'prepatch')).toBe(false);
+  });
+
+  it('should return true when the after version is after the before version with prerelease release type', () => {
+    expect(isNewerVersion('1.0.0', '1.0.1-beta.0', 'prerelease')).toBe(true);
+  });
+
+  it('should return false when the after version is before the before version with prerelease release type', () => {
+    expect(isNewerVersion('1.0.1-beta.0', '1.0.0', 'prerelease')).toBe(false);
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { isNewerVersion } from './version';
+
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const before: string = core.getInput('before', { required: true });
+    const after: string = core.getInput('after', { required: true });
+    const releaseType: string = core.getInput('release_type', { required: true });
+    core.setOutput('newer', isNewerVersion(before, after, releaseType));
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
